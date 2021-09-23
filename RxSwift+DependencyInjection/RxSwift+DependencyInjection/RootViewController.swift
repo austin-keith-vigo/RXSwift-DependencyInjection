f3116766dbfaa87fbc6ce7aa0ad7086ceb6e5a63
austin-vigo-Randstad <Austin_Vigo@bose.com>
2021-09-23 07:39:27 -0700

    init project
    init project

---
+++ RxSwift+DependencyInjection/RxSwift+DependencyInjection/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  RxSwift+DependencyInjection
+//
+//  Created by Vigo, Austin on 9/23/21.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .darkGray
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+
+}
